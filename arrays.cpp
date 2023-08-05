6f2c4f72a7e059d3bb5cc57bf5bc82e3e09fa894
sharrmeen <111696981+sharrmeen@users.noreply.github.com>
2023-08-05 12:49:03 +0530

    Add files via upload

---
+++ arrays.cpp
@@ -0,0 +1,57 @@
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//       const int SIZE =5;
+//       int numbers[SIZE];
+//       int sum=0;
+
+//       cout<<"enter five numbers: ";
+//       for (int i=0;i<SIZE;i++)
+//       {
+//         cin>>numbers[i];
+//       }
+//      for(int i=0;i<SIZE;++i)
+//      {
+//         sum+=numbers[i];
+//      }
+//       cout<<"sum of the numbers: "<<sum<<endl;
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int number[5]= {1,2,3,4,5};
+//     for (int i =0;i<5;++i)
+//     {
+//         cout<<" "<<number[i]<<" ";
+//     }
+
+//     cout<<endl;
+
+// }
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int matrix[3][3]={
+//         {1,2,3},
+//         {4,5,6},
+//         {7,8,9}
+//     };
+
+//     for (int i=0;i<3;++i)
+//     {
+//         for (int j=0;j<3;++j)
+//         {cout<<matrix[i][j]<<" ";}
+//         cout<<endl;
+//     }
+
+     
+// }
+
